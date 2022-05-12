1794df4727ac328dafc20469ef9a40d20b23101f
joaoVictorVictoy <joaovvag@gmail.com>
2022-05-12 17:55:33 -0300

    Exercicio_funcoes

---
+++ modulo1/funcoes/index.js
@@ -0,0 +1,141 @@
+//Exercícios de interpretação de código
+
+// 1.Interpretação
+
+// function minhaFuncao(variavel) {
+// 	return variavel * 5
+// }
+
+// console.log(minhaFuncao(2))
+// console.log(minhaFuncao(10))
+//Essa função imprime 10 e 50, pois a variavel recebe o valor 2 e 10 .
+
+//2. Interpretação
+
+// let textoDoUsuario = prompt("Insira um texto");
+
+// const outraFuncao = function(texto) {
+// 	return texto.toLowerCase().includes("cenoura")
+// }
+
+// const resposta = outraFuncao(textoDoUsuario)
+// console.log(resposta)
+// A.(Execeutei) A função "outraFuncao" busca a sequencia de caracteres string "cenoura" retornando true ou false.
+// B. i.true ii.true iii.true pois todos contém "cenoura"
+
+//Exercícios de escrita de código
+
+//1.Exercicio
+
+//A
+// function sobreMim(){
+//     console.log("Eu sou João, tenho 24 anos, moro em Belo Horizonte e sou estudante")
+
+// }
+// sobreMim()
+
+
+//B
+
+// function sobreMimEditado(nome, idade, cidade, profissao){
+
+//    console.log(`Eu sou ${nome}, tenho ${idade}, moro em ${cidade}, sou ${profissao}.`)
+
+// }
+// sobreMimEditado("joao", 24, "BH", "Estudante")
+
+
+//2.Exercicio
+
+//A
+
+// function somaNumeros(num1=0, num2=0){
+
+// return num1+num2
+
+// }
+// console.log(somaNumeros(6, 6))
+
+// const somaNumeros = (a, b) => a+b
+
+// console.log(somaNumeros(4,10))
+
+//B
+
+// const maiorIgual = (num, num1 ) => num>= num1
+
+// const validar = maiorIgual
+
+// console.log(maiorIgual(1, 5))
+
+//C
+
+// const parOuNao = (n) => {
+
+// const par = n%2
+// return par === 0
+// }
+// console.log(parOuNao(45))
+
+//D 
+
+// function mensagem(texto){
+
+//     let mensagemUsuario = prompt("Escreva uma mensagem:")
+//     const tamanho = mensagemUsuario.length
+    
+//     return tamanho
+// }
+// console.log(mensagem())
+
+
+//3.Exercicio
+
+
+// function operacoesBasicas(num, num1){
+
+//  num =+prompt("Digite um numero:")
+//   num1 =+prompt("Digite outro um numero:")
+     
+
+//     const soma = num + num1
+//     const diferenca = num - num1
+//     const multi = num * num1
+//     const div = num/num1
+    
+//     console.log(`Soma: ${soma}, Diferença: ${diferenca}, Multiplicação: ${multi}, Divisão: ${div}`)
+//     return num, num1
+// }
+// operacoesBasicas()
+
+
+//🏅  Desafios
+
+//A 
+
+// const recebeParametro = (B) =>{
+
+//     const parametro = B/2
+//  return parametro
+
+// }
+// console.log(recebeParametro(40))
+
+
+//B
+
+// const soma = (num, num1 ) => num+num1
+
+// const validar = soma
+
+// console.log(soma(10, 10)
+
+
+//2.
+// function pitagoras(){
+
+
+
+// }
+
+
