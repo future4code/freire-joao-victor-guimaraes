a6d6618d731fe0b9615066029baa58f9fda51f11
joaoVictorVictoy <joaovvag@gmail.com>
2022-06-12 14:35:08 -0300

    tentativa

--- modulo2/projeto-whatslab/whatslab/src/App.js
+++ modulo2/projeto-whatslab/whatslab/src/App.js
@@ -1,25 +1,73 @@
-import logo from './logo.svg';
+
 import './App.css';
+// import styled from 'styled-components';
+import React from "react";
+// import Mensagens from "./components//Mensagens";
+class App extends React.Component{
 
-function App() {
+    state = {
+        Mensagens: [
+            {
+              remetente: "",
+               mensagem: ""
+            }
+        ],
+        valorInputRemetente: "",
+        valorInputMensagem: ""
+    }
+    addMensagem = () => {
+        const novaMensagem = {
+            remetente: this.state.valorInputRemetente,
+            mensagem: this.state.valorInputMensagem
+
+        };
+
+        const novasMensagens = [this.state.Mensagens, novaMensagem];
+
+        this.setState({ Mensagens: novasMensagens });
+        this.setState({ valorInputMensagem: "", valorInputRemetente: "" });
+
+    };
+    onChangeInputRemetente = (event) => { this.setState({ valorInputRemetente: event.target.value }) };
+    onChangeInputMensagem = (event) => { this.setState({ valorInputMensagem: event.target.value }) };
+
+
+
+
+
+render() {
+  const mensagem = this.state.Mensagens.map((mens) => {
+    return (
+        <p> {mens.mensagem}    </p>     
+    );   
+});
+
+const remetente = this.state.Mensagens.map((mens) => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+       <b>    {mens.remetente}     </b>        
   );
-}
 
+  
+});
+
+return <div className='container'>
+        <div className='containerDiv'>
+        
+            <div className='containerMensagens'>
+             <div>{remetente}{mensagem}</div>
+                
+            </div>
+            
+            <form>
+                <input className='remetente' value={this.state.valorInputRemetente} onChange={this.onChangeInputRemetente} placeholder={" Remetente"} ></input>
+                <input className='mensagem' value={this.state.valorInputMensagem} onChange={this.onChangeInputMensagem} placeholder={" Mensagem"} ></input>
+                <button className='botaoEnviar' onClick={this.addMensagem}> Send </button>
+
+            </form>
+        </div>
+        
+    </div>
+
+}
+}
 export default App;
