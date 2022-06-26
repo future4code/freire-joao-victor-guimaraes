5967fc327abba827f478f128cccacf886ad445b4
joaoVictorVictoy <joaovvag@gmail.com>
2022-06-26 08:50:51 -0300

    implementação html

---
+++ modulo2/labefy/labefy/src/Components/TelaInicial/TelaInicial.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import {
+    ContainerInfo,
+    Head,
+    CompHead,
+    Body
+  } from "./styled";
+
+const TelaInicial = (props) => {
+
+
+    return (
+        <ContainerInfo>
+            <Head>
+                <h1>Labefy</h1>
+                <CompHead>
+                <h2>Todas Playlists</h2>
+                
+                <h2>Criar Playlist</h2>
+                </CompHead>
+                </Head>
+            
+            <Body>
+
+            </Body>
+
+        </ContainerInfo>
+
+    );
+};
+
+export default TelaInicial ;
---
+++ modulo2/labefy/labefy/src/Components/TelaInicial/styled.js
@@ -0,0 +1,29 @@
+import styled from "styled-components";
+
+export const ContainerInfo = styled.div`
+  /* display: flex;
+  flex-direction: row;
+ height: 100vh;
+  align-items: center;
+  background-color:#545e55 ; */
+`;
+
+
+export const Head = styled.div`
+  background-color: #30503a ;
+  display: flex;
+
+  justify-content: space-between;
+   align-items: center;
+   padding: 0 20px 0 20px ;
+ 
+`
+export const CompHead = styled.div`
+ padding: 0 20px 0 20px ;
+  display: flex;
+  
+`
+export const Body = styled.div`
+  height: 100vh;
+  background-color:#545e55 ; 
+`
