5166ad8f7165ea616005e0956797e8614e8a1250
joaoVictorVictoy <joaovvag@gmail.com>
2022-07-05 18:12:03 -0300

    atividade-useEffects

---
+++ modulo2/aula32-use-effect/src/App.js
@@ -0,0 +1,20 @@
+
+import './App.css';
+
+
+import Usepoke from './Components/UsePoke';
+
+function App() {
+  return (
+    <div className='App'>
+      
+     <Usepoke/>
+      <hr/>
+      
+
+    </div>
+  )
+
+}
+
+export default App;
