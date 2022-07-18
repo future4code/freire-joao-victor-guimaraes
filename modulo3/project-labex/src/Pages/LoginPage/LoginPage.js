e9198cd2b1c462954de6154c977a9b8838461caa
joaoVictorVictoy <joaovvag@gmail.com>
2022-07-18 18:04:26 -0300

    tentativa de implementar o useForm

--- modulo3/project-labex/src/Pages/LoginPage/LoginPage.js
+++ modulo3/project-labex/src/Pages/LoginPage/LoginPage.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import  axios from "axios";
+import axios from "axios";
 import { URL_BASE } from "../../Constantes/URL_BASE";
 import { useNavigate } from "react-router-dom"
 import { goBack } from "../../Routes/Coordinator";
@@ -8,30 +8,42 @@ import { goBack } from "../../Routes/Coordinator";
 export function LoginPage() {
     const navigate = useNavigate();
 
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
+    cosnt [email, setEmail] = useState("")
+    cosnt [password, setPassword] = useState("")
 
-    const onChangeEmail = (e) => {
-        setEmail(e.target.value);
+    // // Aqui tentei utilizar o Form Operator
+    // const [form, setForm] = useState({ email: "", password: "" })
+
+
+    const changeEmail=(e)=>{
+        setEmail(e.target.value)
+    }
+    const changePassword=(e)=>{
+        setPassword(e.target.value)
     }
 
-    const onChangePassword = (e) => {
-        setPassword(e.target.value);
-    }
 
+    // Aqui tentei utilizar o Form Operator
+    // const requestForm = (e) => {
+    //     // const {name,value} = e.target
+    //     setForm({ ...form, [e.target.name]: e.target.value })
+    //     // setForm({...form,[name]:value})
+    // }
 
-    const Login = () => {
-        console.log(email, password);
+    const Login = (e) => {
+        e.prevetDefault()
+
+        
         const body = {
-            email: email,
-            password: password
-        };
-       
-       axios
-        .post( 
-            `${URL_BASE}/login`, 
-        body
-        )
+            email:email,
+            passoword:password
+        }
+
+        axios
+            .post(
+                `${URL_BASE}/login`,
+                body
+            )
 
             .then((res) => {
                 console.log("Deu certo: ", res.data);
@@ -48,32 +60,35 @@ export function LoginPage() {
 
     return (
         <div>
-            <p>Area login</p>
-
-            <label>E-mail:</label>
-            <input
-                placeholder="Digite seu email"
-                type="email"
-                value={email}
-                onChange={onChangeEmail}
-
-            />
-            <br /> <br />
-
-            <label>Password:</label>
-            <input
-                placeholder="Password"
-                type="password"
-                value={password}
-                onChange={onChangePassword}
-            /> <br />
-            <br />
-            <br />
+            <h1>Area login</h1>
 
 
+
+               <input
+                    name="email"
+                    placeholder="Digite seu email"
+                    type="email"
+                    value={email}
+                    onChange={changeEmail}
+                    required
+                />
+                <br /> <br />
+
+
+                <input
+                    name="password"
+                    placeholder="Password"
+                    // type="password"
+                    value={password}
+                    onChange={changePassword}
+                    required
+                />
+
+                <br />    <br />    <br />
+                <button onClick={Login}>Login</button>
+
+            
             <button onClick={() => goBack(navigate)}>Voltar</button>
-            <button onClick={Login}>Login</button>
-
         </div>
     )
 
