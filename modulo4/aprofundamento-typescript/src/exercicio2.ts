181ea8e49e52832d6aa6e77d476aed926b99260f
joaoVictorVictoy <joaovvag@gmail.com>
2022-08-20 11:40:47 -0300

    exercicio2

---
+++ modulo4/aprofundamento-typescript/src/exercicio2.ts
@@ -0,0 +1,30 @@
+console.log(
+  "A. Entrada Parametro de *numeros*, e podem ser ordenados pelo *sort*, que adiciona num a numeros e retorna valor com o numero adicionado, e ao final mostra nos maior menor e media do numeros."
+);
+
+function obterEstatisticas(numeros: number[]): Estat {
+  const numerosOrdenado: number[] = numeros.sort((a, b) => a - b);
+
+  let soma: number = 0;
+
+  for (let num of numeros) {
+    soma += num;
+  }
+
+  const estatisticas: Estat = {
+    maior: numerosOrdenado[numeros.length - 1],
+    menor: numerosOrdenado[0],
+    media: soma / numeros.length,
+  };
+
+  return estatisticas;
+}
+type Estat = { maior: number; menor: number; media: number };
+console.log("B. numeros:number[], numerosOrdenados:number, soma: number   ");
+
+type AmostraDeIdades = {
+  numeros: number[]
+  obterEstatisticas: (numeros: number[]) => Estat;
+};
+
+console.log(obterEstatisticas([21, 18, 65, 44, 15, 18]))
--- modulo4/aprofundamento-typescript/src/index.js
+++
@@ -1,3 +0,0 @@
-"use strict";
-console.log("Olá Pessoa Corretora");
-//# sourceMappingURL=index.js.map
