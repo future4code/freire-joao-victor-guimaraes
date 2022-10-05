b17bcec4e3b89c8327ce118fc7ce09f83841cfce
joaoVictorVictoy <joaovvag@gmail.com>
2022-10-05 13:42:28 -0300

    Criado GlobalContext-utilizando Tarot.json/Pasta-Card/Pasta-Header

---
+++ modulo6/case-tarot-personare/src/context/cardContext.js
@@ -0,0 +1,35 @@
+import { useState, useEffect, createContext } from "react";
+import axios from "axios";
+
+
+export const CardContext = createContext();
+
+const GlobalProvider = ({ children }) => {
+  const [ card, setCard ]= useState([]);
+  const [path, setPath] = useState([]);
+  const [flip, setFlip] = useState(true);
+
+  useEffect(() => {
+    getCard();
+    getPaths();
+  });
+  const getCard = async () => {
+    const res = await axios.get('/tarot.json');
+    setCard(res.data.cards);
+  };
+  const getPaths = async () => {
+    const res = await axios.get('/tarot.json');
+    setPath(res.data);
+  };
+  const handleFlipCard = () => {
+    setFlip(!flip);
+  };
+  const imagePath = path.imagesUrl
+  const backCard = path.imageBackCard
+  return (
+    <CardContext.Provider value={{ card, path, imagePath, backCard, flip, handleFlipCard }}>
+      { children }
+    </CardContext.Provider>
+  );
+};
+export default GlobalProvider;
