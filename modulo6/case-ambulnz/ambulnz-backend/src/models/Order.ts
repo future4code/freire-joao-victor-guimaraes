c3e1e4e75e19641a438ae0ae4c96c231898ad0f4
joaoVictorVictoy <joaovvag@gmail.com>
2022-10-06 10:48:49 -0300

    inicializado

---
+++ modulo6/case-ambulnz/ambulnz-backend/src/models/Order.ts
@@ -0,0 +1,88 @@
+export interface IOrderDB {
+    id: string
+}
+
+export interface IOrderItemDB {
+    id: string,
+    pizza_name: string,
+    quantity: number,
+    order_id: string
+}
+
+export interface IOrderItem {
+    id: string,
+    pizza_name: string,
+    price: number,
+    quantity: number,
+    order_id: string
+}
+
+export interface IOrderResume {
+    id: string,
+    pizzas: {
+        name: string,
+        quantity: number,
+        price: number
+    }[],
+    total: number
+}
+
+export class Order {
+    private total: number = 0
+
+    constructor(
+        private id: string,
+        private orderItems: IOrderItem[],
+    ) {
+        this.total = this.calculateTotal()
+    }
+
+    private calculateTotal = () => {
+        const total = this.orderItems.reduce(
+            (acc, pizza) => acc + (pizza.price * pizza.quantity),
+            0
+        )
+
+        return total
+    }
+    
+    public getId = () => {
+        return this.id
+    }
+    
+    public getOrderItems = () => {
+        return this.orderItems
+    }
+
+    public setOrderItems = (newOrderItems: IOrderItem[]) => {
+        this.orderItems = newOrderItems
+    }
+
+    public addOrderItem = (newOrderItem: IOrderItem) => {
+        this.orderItems.push(newOrderItem)
+    }
+
+    public removeOrderItem = (idToRemove: string) => {
+        return this.orderItems.filter(orderItem => orderItem.id !== idToRemove)
+    }
+
+    public getTotal = () => {
+        return this.total
+    }
+}
+
+export interface ICreateOrderInputDTO {
+    pizzas: {
+        name: string,
+        quantity: number
+    }[]
+}
+
+export interface ICreateOrderOutputDTO {
+    message: string,
+    order: IOrderResume
+}
+
+export interface IGetOrdersOutputDTO {
+    orders: IOrderResume[]
+}
